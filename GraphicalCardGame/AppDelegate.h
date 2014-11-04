a90641f658d895fa9f428782fd365ae95553964a
LuQuan Intrepid <LuQuan@LuQuans-MacBook-Pro.local>
2014-11-04 11:24:49 -0500

    Initial commit of GraphicalCardGame Project.

---
+++ GraphicalCardGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GraphicalCardGame
+//
+//  Created by LuQuan Intrepid on 11/4/14.
+//  Copyright (c) 2014 LuQuan Intrepid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GraphicalCardGame/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GraphicalCardGame
+//
+//  Created by LuQuan Intrepid on 11/4/14.
+//  Copyright (c) 2014 LuQuan Intrepid. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
