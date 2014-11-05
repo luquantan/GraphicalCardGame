282c3551f9ece6c0d1c66dee0e86e15d217f4016
LuQuan Intrepid <LuQuan@LuQuans-MacBook-Pro.local>
2014-11-05 11:29:11 -0500

    Updated PlayingCard and SetCard as a subclass of Card instead of NSObject.

--- GraphicalCardGame/PlayingCard.h
+++ GraphicalCardGame/PlayingCard.h
@@ -1,13 +1,13 @@
 //
-//  SetCard.m
+//  PlayingCard.h
 //  GraphicalCardGame
 //
 //  Created by LuQuan Intrepid on 11/5/14.
 //  Copyright (c) 2014 LuQuan Intrepid. All rights reserved.
 //
 
-#import "SetCard.h"
+#import "Card.h"
 
-@implementation SetCard
+@interface PlayingCard : Card
 
 @end
--- GraphicalCardGame/PlayingCard.m
+++ GraphicalCardGame/PlayingCard.m
@@ -2,14 +2,12 @@
 //  PlayingCard.m
 //  GraphicalCardGame
 //
-//  Created by LuQuan Intrepid on 11/4/14.
+//  Created by LuQuan Intrepid on 11/5/14.
 //  Copyright (c) 2014 LuQuan Intrepid. All rights reserved.
 //
 
 #import "PlayingCard.h"
 
-
 @implementation PlayingCard
-
 
 @end
